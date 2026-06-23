29e8d118089ac2618881281aa3d1f3a5eb398e50
Arnab <arnabchoudhury2005@gmail.com>
2026-06-23 08:53:49 +0530

    Completed work on the navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,41 +1,73 @@
-import React from 'react'
+import React, { useState } from 'react'
 import logo from "../assets/logo.jpg";
 import { IoIosSearch } from "react-icons/io";
 import { CgProfile } from "react-icons/cg";
-import { NavLink } from 'react-router-dom';
+import { FiShoppingCart } from "react-icons/fi";
+import { FiMenu } from "react-icons/fi";
+import { IoIosArrowForward } from "react-icons/io";
+import { Link, NavLink } from 'react-router-dom';
 const Navbar = () => {
-  return (
-    <div className='flex items-center justify-between py-5 front-medium'>
-        <img src={logo} className='w-36' alt="" />
 
-        <ul className='hidden sm:flex gap-5 text-sm text-gary-700'>
-            <NavLink to='/' className='flex flex-col items-center gap-1'>
-                <p>HOME</p>
-                <hr className='w-2/4 border-none h-[1.5px] bg-gray-700 hidden'/>
-            </NavLink>
-            <NavLink to='/collection' className='flex flex-col items-center gap-1'>
-                <p>COLLECTION</p>
-                <hr className='w-2/4 border-none h-[1.5px] bg-gray-700 hidden'/>
-            </NavLink>
-            <NavLink to='/about' className='flex flex-col items-center gap-1'>
-                <p>ABOUT</p>
-                <hr className='w-2/4 border-none h-[1.5px] bg-gray-700 hidden'/>
-            </NavLink>
-            <NavLink to='/contact' className='flex flex-col items-center gap-1'>
-                <p>CONTACT</p>
-                <hr className='w-2/4 border-none h-[1.5px] bg-gray-700 hidden'/>
-            </NavLink>
-        </ul>
+    const [visible, setVisible] = useState(false);
 
-        <div className='flex item-center gap-6'>
-            <IoIosSearch className='w-5 h-5 cursor-pointer' />
+    return (
+        <div className='flex items-center justify-between py-5 front-medium'>
+            <img src={logo} className='w-36' alt="" />
 
-            <div className='group relative'>
-                <CgProfile  className='w-5 h-5 cursor-pointer'/>
+            <ul className='hidden sm:flex gap-5 text-sm text-gary-700'>
+                <NavLink to='/' className='flex flex-col items-center gap-1'>
+                    <p>HOME</p>
+                    <hr className='w-2/4 border-none h-[1.5px] bg-gray-700 hidden' />
+                </NavLink>
+                <NavLink to='/collection' className='flex flex-col items-center gap-1'>
+                    <p>COLLECTION</p>
+                    <hr className='w-2/4 border-none h-[1.5px] bg-gray-700 hidden' />
+                </NavLink>
+                <NavLink to='/about' className='flex flex-col items-center gap-1'>
+                    <p>ABOUT</p>
+                    <hr className='w-2/4 border-none h-[1.5px] bg-gray-700 hidden' />
+                </NavLink>
+                <NavLink to='/contact' className='flex flex-col items-center gap-1'>
+                    <p>CONTACT</p>
+                    <hr className='w-2/4 border-none h-[1.5px] bg-gray-700 hidden' />
+                </NavLink>
+            </ul>
+
+            <div className='flex item-center gap-6'>
+                <IoIosSearch className='w-5 h-5 cursor-pointer' />
+
+                <div className='group relative'>
+                    <CgProfile className='w-5 h-5 cursor-pointer' />
+                    <div className='group-hover:block hidden absolute dropown-menu right-0 pt-4'>
+                        <div className='flex flex-col gap-2 w-36 py-3 px-5 bg-slate-100 text-gray-500 rounded'>
+                            <p className='cursor-pointer hover:text-black'>My Profile</p>
+                            <p className='cursor-pointer hover:text-black'>Orders</p>
+                            <p className='cursor-pointer hover:text-black'>Logout</p>
+                        </div>
+                    </div>
+                </div>
+                <Link to='/cart' className='relative'>
+                    <FiShoppingCart  className='w-5 min-w-5 h-5'/>
+                    <p className='absolute right-[-5px] bottom-[-5px] w-4 text-center leading-4 bg-black text-white aspect-square rounded-full text-[8px]'>10</p>
+                </Link>
+                <FiMenu onClick={()=> setVisible(true)} className='w-5 cursor-pointer sm:hidden'/>
+            </div>
+
+            {/* Sidebar menu for small screen */}
+            <div className={`absolute top-0 right-0 bottom-0 overflow-hidden bg-white transition-all ${visible ? 'w-full' : 'w-0'}`}>
+                <div className='flex flex-col text-gray-600'>
+                    <div onClick={()=>setVisible(false)} className='flex items-center gap-4 p-3 cursor-pointer'>
+                        <IoIosArrowForward className='h-4 rotate-180'/>
+                        <p>Back</p>
+                    </div>
+                    <NavLink onClick={()=>setVisible(false)} className='py-2 pl-6 border' to='/'>HOME</NavLink>
+                    <NavLink onClick={()=>setVisible(false)} className='py-2 pl-6 border' to='/collection'>COLLECTION</NavLink>
+                    <NavLink onClick={()=>setVisible(false)} className='py-2 pl-6 border' to='/about'>ABOUT</NavLink>
+                    <NavLink onClick={()=>setVisible(false)} className='py-2 pl-6 border' to='/contact'>CONTACT</NavLink>
+                </div>
             </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default Navbar
